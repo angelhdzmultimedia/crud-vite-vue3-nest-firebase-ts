70a12f5e875348df7c2a8d95a3d0ff0c7dbcc004
angelhdzmultimedia <angel.hernandez.12@live.com>
2022-08-05 20:34:39 -0400

    1 file modified

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { Redirect } from '@nestjs/common/decorators/http/redirect.decorator';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import { Res } from '@nestjs/common/decorators/http/route-params.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Redirect('https://node-9dvwhs--5000.local.webcontainer.io', 200)
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: Response): void {
+    return res.redirect('https://node-9dvwhs--5000.local.webcontainer.io');
   }
 }
